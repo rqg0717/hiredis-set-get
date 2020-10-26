02e99573e4c7f9ef7fc4697e0275a42f236f8f60
James Ren <rqg0717@gmail.com>
2020-10-26 17:06:44 -0500

    Update example.c

--- example.c
+++ example.c
@@ -2,12 +2,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <time.h>
 #include <string.h>
 #include <inttypes.h>
 
 #include <hiredis/hiredis.h>
 
-#define MSG_SIZE    4096
+#define MSG_SIZE    0x1000
 
 typedef struct TEST {
     uint32_t ui;
@@ -23,8 +24,8 @@ const char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
     'v', 'w', 'x', 'y', 'z'};
 
 void set(DATA *data) {
-    const char *commands[3];
-    size_t lens[3];
+    //const char *commands[3];
+    //size_t lens[3];
     redisContext *rc = NULL;
     redisReply *rr = NULL;
 
@@ -39,21 +40,22 @@ void set(DATA *data) {
         return;
     }
 
-    commands[0] = "set";
-    lens[0] = strlen("set");
-
-    commands[1] = "data";
-    lens[1] = strlen("data");
-
-    commands[2] = (const char *) data;
-    lens[2] = sizeof (DATA);
-
-    rr = (redisReply *) redisCommandArgv(rc, sizeof (commands) / sizeof (commands[0]), commands, lens);
-    if (rr == NULL) {
-        printf("empty reply\n");
-        goto EXIT;
-
-    }
+//    commands[0] = "set";
+//    lens[0] = strlen("set");
+//
+//    commands[1] = "data";
+//    lens[1] = strlen("data");
+//
+//    commands[2] = (const char *) data;
+//    lens[2] = sizeof (DATA);
+//
+//    rr = (redisReply *) redisCommandArgv(rc, sizeof (commands) / sizeof (commands[0]), commands, lens);
+//    if (rr == NULL) {
+//        printf("empty reply\n");
+//        goto EXIT;
+//
+//    }
+    rr = redisCommand(rc, "SET data %b", data, (size_t) sizeof (DATA));
     printf("reply: %s\n", rr->str);
 
 EXIT:
@@ -65,6 +67,7 @@ EXIT:
         redisFree(rc);
         rc = NULL;
     }
+
 }
 
 void get(void) {
@@ -89,7 +92,7 @@ void get(void) {
         goto EXIT;
     }
 
-    printf("reply length: %d. \n", (int) rr->len);
+    printf("reply length: %lu. \n", rr->len);
     if (rr->len != sizeof (DATA)) {
         printf("reply length is wrong!!! \n");
         goto EXIT;
@@ -105,6 +108,7 @@ void get(void) {
         for (i = 0; i < MSG_SIZE; i++) {
             printf("%c", data->msg[i]);
         }
+        printf("\n");
     }
 
     if (data) {
@@ -113,6 +117,116 @@ void get(void) {
     }
 
 EXIT:
+    if (rr) {
+        freeReplyObject(rr);
+        rr = NULL;
+    }
+    if (rc) {
+        redisFree(rc);
+        rc = NULL;
+    }
+
+}
+
+void zadd(DATA *data) {
+    const char *commands[4];
+    size_t lens[4];
+    redisContext *rc = NULL;
+    redisReply *rr = NULL;
+    char *score = NULL;
+    
+    time_t now = time(NULL);
+    asprintf( &score, "%lu", now);
+    
+    rc = redisConnect("127.0.0.1", 6379);
+    if (rc == NULL || rc->err) {
+        if (rc) {
+            printf("Redis Error: %s. \n", rc->errstr);
+            /* handle error here */
+        } else {
+            printf("Can't allocate Redis context! \n");
+        }
+        return;
+    }
+
+    commands[0] = "zadd";
+    lens[0] = strlen("zadd");
+
+    commands[1] = "msg";
+    lens[1] = strlen("msg");
+
+    commands[2] = score;
+    lens[2] = strlen(score);
+    
+    commands[3] = (const char *) data;
+    lens[3] = sizeof (DATA);
+
+    rr = (redisReply *) redisCommandArgv(rc, sizeof (commands) / sizeof (commands[0]), commands, lens);
+
+EXIT:
+    if (score) {
+        free(score);
+        score = NULL;
+    }
+    if (rr) {
+        freeReplyObject(rr);
+        rr = NULL;
+    }
+    if (rc) {
+        redisFree(rc);
+        rc = NULL;
+    }
+}
+
+void zget(void) {
+    int i = 0;
+    size_t sz = 0;
+    DATA *data = NULL;
+    redisContext *rc = NULL;
+    redisReply *rr = NULL;
+
+    rc = redisConnect("127.0.0.1", 6379);
+    if (rc == NULL || rc->err) {
+        if (rc) {
+            printf("Redis Error: %s. \n", rc->errstr);
+        } else {
+            printf("Can't allocate Redis context! \n");
+        }
+        return;
+    }
+
+    rr = (redisReply *) redisCommand(rc, "zrange msg 0 -1 WITHSCORES");
+    if (rr == NULL) {
+        printf("empty reply!!!\n");
+        goto EXIT;
+    }
+
+    printf("reply elements: %lu. \n", rr->elements);
+    
+    data = (DATA *) malloc(sizeof (DATA));
+    if (data == NULL) {
+        printf("Can't allocate...\n");
+    } else {
+        for (sz = 0; sz < rr->elements; sz++) {
+            if (rr->element[sz]->len != sizeof (DATA)) {
+                printf("score is: %s. \n", rr->element[sz]->str);
+                continue;
+            }
+            memcpy(data, rr->element[sz]->str, rr->element[sz]->len);
+            printf("Get data: %d, %u, %" PRIu64 ", %" PRId64 ". \n", data->i, data->ui, data->ul, data->l);
+            printf("Message is :");
+            for (i = 0; i < MSG_SIZE; i++) {
+                printf("%c", data->msg[i]);
+            }
+            printf("\n");
+        }
+    }
+    
+EXIT:
+    if (data) {
+        free(data);
+        data = NULL;
+    }
     if (rr) {
         freeReplyObject(rr);
         rr = NULL;
@@ -136,6 +250,21 @@ int main(int argc, char** argv) {
 
     set(&data);
     get();
+    
+    zadd(&data);
+    sleep(1);
+    
+    for (i = 0; i < MSG_SIZE; i++) {
+        data.msg[i] = alphabet[rand() % 26];
+    }
+    zadd(&data);
+    sleep(1);
+    
+    for (i = 0; i < MSG_SIZE; i++) {
+        data.msg[i] = alphabet[rand() % 26];
+    }
+    zadd(&data);
+    zget();
 
     return (EXIT_SUCCESS);
 }
